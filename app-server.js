ab69d090a9c031b8b802eaa8e1be61e275ebfa1b
William Jackson <williampj@gmail.com>
2020-07-23 23:12:51 +0300

    changed for demo

--- app-server.js
+++ app-server.js
@@ -1,26 +1,34 @@
-const http = require("http");
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const path = require("path");
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+const axios = require('axios');
 
 const app = express();
 const todos = [];
 
-const staticPath = path.join(__dirname, "public");
+const staticPath = path.join(__dirname, 'public');
 app.use(express.static(staticPath));
 
-app.use(logger("tiny"));
+app.use(logger('tiny'));
 
 app.use(bodyParser.json());
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   const todo = req.body.todo;
   todos.push(todo);
+
+  axios
+    .post('http://localhost:8181/api', {
+      data: todo,
+    })
+    .catch((e) => console.log(e));
+
   res.json(req.body);
 });
 
 const server = http.createServer(app);
 server.listen(3000, () => {
-  console.log("Server started on port 3000");
+  console.log('Server started on port 3000');
 });
--- public/script.js
+++ public/script.js
@@ -1,23 +1,36 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  const inputField = document.getElementById("todo");
-  const list = document.getElementById("todos");
+const ws = new WebSocket('ws://localhost:8181');
 
-  form.addEventListener("submit", (e) => {
+ws.onopen = function (e) {
+  console.log('Connection to server opened');
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+  const inputField = document.getElementById('todo');
+  const list = document.getElementById('todos');
+
+  ws.onmessage = (event) => {
+    console.log(event);
+    const item = document.createElement('li');
+    item.innerHTML = JSON.parse(event.data).data;
+    list.append(item);
+  };
+
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const todo = { todo: inputField.value };
 
-    fetch("/", {
-      method: "post",
+    fetch('/', {
+      method: 'post',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(todo),
     })
       .then((response) => response.json())
       .then((data) => {
-        const item = document.createElement("li");
+        const item = document.createElement('li');
         item.innerHTML = data.todo;
         list.append(item);
       })
